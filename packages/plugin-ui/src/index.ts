8a5d3a00b9e75b32e5c2cdf860ddde5e3ffae88c
FruitJ <1848661762@qq.com>
2026-05-27 12:03:42 +0800

    feat: 创建 ui 插件

---
+++ packages/plugin-ui/src/index.ts
@@ -0,0 +1,10 @@
+export { message } from "./message";
+export type { MessageType, MessageConfig } from "./message";
+
+export { createConfirmDialog } from "./confirm-dialog";
+export type {
+  AIAnswerOption,
+  AIResponse,
+  ConfirmDialogOptions,
+  ConfirmDialog,
+} from "./confirm-dialog";
